93bc075f9e32344b31fdb69137e588aa17c876ff
Luke Downing <lukeraymonddowning@gmail.com>
2022-08-25 16:53:41 +0100

    Hides certain environment variables from the runtime.

--- src/Actions/RunPestTests.php
+++ src/Actions/RunPestTests.php
@@ -19,11 +19,47 @@ final class RunPestTests implements RunsPestTests
     {
         $process = new Process(
             [$this->fileSystem->phpBinary(), 'vendor/bin/pest'],
-            $this->fileSystem->projectRoot()
+            $this->fileSystem->projectRoot(),
+            $this->hiddenEnvironmentVariables(),
         );
 
         $process->run();
 
         return new Output($process->getExitCode(), $process->getOutput(), $process->getErrorOutput());
+    }
+
+    /**
+     * @return array<string, false>
+     */
+    private function hiddenEnvironmentVariables(): array
+    {
+        return [
+            '_AWS_XRAY_DAEMON_ADDRESS' => false,
+            '_AWS_XRAY_DAEMON_PORT' => false,
+            '_HANDLER' => false,
+            'AWS_ACCESS_KEY_ID' => false,
+            'AWS_DEFAULT_REGION' => false,
+            'AWS_LAMBDA_FUNCTION_NAME' => false,
+            'AWS_LAMBDA_FUNCTION_VERSION' => false,
+            'AWS_LAMBDA_FUNCTION_MEMORY_SIZE' => false,
+            'AWS_LAMBDA_INITIALIZATION_TYPE' => false,
+            'AWS_LAMBDA_LOG_GROUP_NAME' => false,
+            'AWS_LAMBDA_LOG_STREAM_NAME' => false,
+            'AWS_LAMBDA_RUNTIME_API' => false,
+            'AWS_REGION' => false,
+            'AWS_REQUEST_ID' => false,
+            'AWS_SECRET_ACCESS_KEY' => false,
+            'AWS_SESSION_TOKEN' => false,
+            'AWS_XRAY_CONTEXT_MISSING' => false,
+            'AWS_XRAY_DAEMON_ADDRESS' => false,
+            'EVALUATOR_DRIVER' => false,
+            'LAMBDA_RUNTIME_DIR' => false,
+            'LAMBDA_TASK_ROOT' => false,
+            'LANG' => false,
+            'LD_LIBRARY_PATH' => false,
+            'PATH' => false,
+            'SHLVL' => false,
+            'SIDECAR_CHECKSUM' => false,
+        ];
     }
 }
